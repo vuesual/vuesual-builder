05315477672d7c63e1136f5b7009566fb9588001
NataliaTepluhina <tarya.se@gmail.com>
2018-08-22 09:19:09 +0300

    * added basic error handler

---
+++ src/components/Error.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>This component is missing from your components library</p>
+</template>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
+
--- src/services/structureConverter.ts
+++ src/services/structureConverter.ts
@@ -1,11 +1,26 @@
 import VuesualComponent from '@/typings/structure';
 import { Component } from 'vue';
+import Error from '@/components/Error.vue';
+import Vue from 'vue';
 
-export type ImportComponentFunction = () => Promise<Component>;
+Vue.config.warnHandler = msg => {
+  console.log(msg.substring(msg.indexOf('Cannot'), msg.length - 1));
+};
+
+interface AsyncComponent {
+  component: Promise<Component>;
+  error: Component;
+  timeout: number;
+}
+
+export type ImportComponentFunction = () => AsyncComponent;
 
 export const toElementComponent = (
   structure: VuesualComponent,
 ): ImportComponentFunction => {
-  const comp = () => import(`@/components/${Object.keys(structure)[0]}.vue`);
-  return comp;
+  return () => ({
+    component: import(`@/components/${Object.keys(structure)[0]}.vue`),
+    error: Error,
+    timeout: 200,
+  });
 };
